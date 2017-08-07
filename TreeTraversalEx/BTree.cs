63da968d713c281647f2f4caea531edea4671134
Ctbram <ctbram@gmail.com>
2017-08-07 11:59:50 -0400

    fixed output of breadthwiseprint

--- TreeTraversalEx/BTree.cs
+++ TreeTraversalEx/BTree.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace TreeTraversalEx
 {
@@ -184,6 +185,7 @@ namespace TreeTraversalEx
                     q.Enqueue(null);
                 }
             }
+            Console.WriteLine();
         }
     }
 }
